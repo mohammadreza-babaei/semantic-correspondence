b4c4bdff0b129f317f6375821fed5a29351c555f
ranfdev <ranfdev@gmail.com>
2025-12-23 17:17:12 +0100

    set shuffle true

--- src/trainer.py
+++ src/trainer.py
@@ -286,7 +286,7 @@ class Trainer():
         train_loader = DataLoader(
             train_dataset,
             batch_size=batch_size, 
-            shuffle=False,
+            shuffle=True,
             num_workers=0, 
             collate_fn=self.model._collate_fn
         )
